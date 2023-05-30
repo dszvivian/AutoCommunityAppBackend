80b66156834df0277a25aa9a0ab2d67cae5cea26
dszvivian <dszvivian@gmail.com>
2023-05-30 11:08:05 +0530

    Database connected successfully! Format the .env file

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,23 @@
 var express = require('express');
 var router = express.Router();
+var mongoose = require('mongoose')
+require('dotenv').config();
+
+const mongoString = "mongodb+srv://vivian:Jo9r2UonQ5dBfNeX@autocommunity.guejjvu.mongodb.net/";
+
+const start = async ()=>{
+
+  try{
+    await mongoose.connect(mongoString)
+    console.log('MongoDb Altas Connected Successfully')
+  }catch(error){
+    console.log(error)
+    process.exit(1)
+  }
+}
+
+start()
+
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
